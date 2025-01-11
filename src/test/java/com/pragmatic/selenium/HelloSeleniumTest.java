00e07b7f3efbbe0f3de47ab8824c4ab0609056a2
thara-1997 <thathsaraninayanathara316@gmail.com>
2025-01-11 09:28:07 +0530

    Initial Commit Commit Two

--- src/test/java/com/pragmatic/selenium/HelloSeleniumTest.java
+++ src/test/java/com/pragmatic/selenium/HelloSeleniumTest.java
@@ -4,17 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class HelloSeleniumTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void openTestLink(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
     @Test
     public void testHelloSelenium(){
-        //create browser instance
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        //navigate to login page
-        driver.get("https://www.saucedemo.com/");
 
         //Type Username
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -31,6 +33,52 @@ public class HelloSeleniumTest {
 
         //close the browser
        // driver.quit();
+    }
+    @Test
+    public void loginWithBlankCredentials(){
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Username is required");
+    }
+
+    @Test
+    public void loginWithBlankUsername(){
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Username is required");
+    }
+    @Test
+    public void loginWithBlankPassword(){
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Password is required");
+    }
+
+    @Test
+    public void loginWithInvalidCredentials(){
+        driver.findElement(By.id("user-name")).sendKeys("ABCD");
+        driver.findElement(By.id("password")).sendKeys("abcd");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void loginWithInvalidPassword(){
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("abcd");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void loginWithInvalidUsername(){
+        driver.findElement(By.id("user-name")).sendKeys("ABCD");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("h3[data-test='error']")).getText(),"Epic sadface: Username and password do not match any user in this service");
     }
 
 
