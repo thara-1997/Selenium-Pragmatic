c8e840362efdd637cf497837b91d4e5b4e403dd7
thara-1997 <thathsaraninayanathara316@gmail.com>
2025-01-23 22:49:54 +0530

    Initial Commit Commit Two

--- src/test/java/com/pragmatic/selenium/hw/CheckoutProcess.java
+++ src/test/java/com/pragmatic/selenium/hw/CheckoutProcess.java
@@ -2,11 +2,15 @@ package com.pragmatic.selenium.hw;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CheckoutProcess {
     WebDriver driver;
@@ -18,9 +22,6 @@ public class CheckoutProcess {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-
-        String errorMessage=driver.findElement(By.cssSelector("[data-test='title']")).getText();
-        Assert.assertEquals(errorMessage,"Products","Product text not meet");
     }
 
     @Test(dataProvider = "information-details")
@@ -32,8 +33,9 @@ public class CheckoutProcess {
 
         driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys(firstname);
         driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys(lastName);
-        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys(postalCode);
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys(postalCode);
         driver.findElement(By.xpath("//input[@id='continue']")).click();
+
         String errorMessage = driver.findElement(By.xpath("//div[@class='error-message-container error']")).getText();
         Assert.assertEquals(errorMessage,expectedMessage,"Error message is incorrect");
     }
@@ -41,11 +43,84 @@ public class CheckoutProcess {
        @DataProvider(name ="information-details")
    public Object[][] userCredentials(){
         return new Object[][]{
-           {"","","","Error: First Name is required"} ,
-           {"Thathsarani", "","","Error: Last Name is required"},
+                {"","","","Error: First Name is required"} ,
+                {"Thathsarani", "","","Error: Last Name is required"},
                 {"Thathsarani","Sudusinghe","","Error: Postal Code is required"},
-                {"Thathsarani","","82100","Error: Last Name is required"}
+                {"Thathsarani", "", "82100", "Error: Last Name is required"}
 
        };
    }
+
+    @Test
+    public void testVerifyValidCheckoutInformationFunctionality() {
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']")).click();
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+
+        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("Thathsarani");
+        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys("Nayanathara");
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@id='continue']")).click();
+
+        Assert.assertTrue(driver.getCurrentUrl().startsWith("https://www.saucedemo.com/checkout-step-two.html"));
+        
+    }
+
+    @Test
+    public void testVerifyOrderSummaryPage() {
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']")).click();
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+
+        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("Thathsarani");
+        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys("Nayanathara");
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@id='continue']")).click();
+
+        List<WebElement> cartItems = driver.findElements(By.xpath("//div[@class='inventory_item_name']"));
+
+        List<String> expectedCartItems = Arrays.asList("Sauce Labs Backpack","Sauce Labs Bike Light");
+
+        Assert.assertEquals(cartItems.size(),expectedCartItems.size(), "cart Items are not matching");
+
+        for (int i =0; i<cartItems.size(); i++){
+            String cartItemName = cartItems.get(i).getText();
+            String expectedProductName = expectedCartItems.get(i);
+            Assert.assertEquals(cartItemName,expectedProductName, "Cart Items are not matching");
+        }
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='summary_total_label']")).getText(),"Total: $43.18", "Total Value is not matching");
+
+    }
+
+    @Test
+    public void testVerifyFinishButtonFunctionality() {
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']")).click();
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+
+        driver.findElement(By.xpath("//input[@id='first-name']")).sendKeys("Thathsarani");
+        driver.findElement(By.xpath("//input[@id='last-name']")).sendKeys("Nayanathara");
+        driver.findElement(By.xpath("//input[@id='postal-code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@id='continue']")).click();
+        driver.findElement(By.xpath("//button[@id='finish']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//h2[normalize-space()='Thank you for your order!']")).getText(),"Thank you for your order!");
+    }
+
+    @Test
+    public void testVerifyCancelButtonFunctionality() {
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-bike-light']")).click();
+        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
+        driver.findElement(By.xpath("//button[@id='checkout']")).click();
+
+
+        driver.findElement(By.xpath("//button[@id='cancel']")).click();
+        Assert.assertTrue(driver.getCurrentUrl().startsWith("https://www.saucedemo.com/cart.html"));
+    }
 }
+
---
+++ src/test/java/com/pragmatic/selenium/hw/ProductListPage.java
@@ -0,0 +1,70 @@
+package com.pragmatic.selenium.hw;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ProductListPage {
+    WebDriver driver;
+    @BeforeMethod
+    public void openTestLink(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Test
+    public void testVerifyAllProductsAreDisplayingInCorrectNameAndPrice() {
+        List<WebElement> productListItems =driver.findElements(By.xpath("//div[@class='inventory_item']"));
+
+        String[][] expectedProducts = {
+                {"Sauce Labs Backpack", "$29.99", "sauce-backpack-1200x1500.0a0b85a3.jpg"},
+                {"Sauce Labs Bike Light", "$9.99", "bike-light-1200x1500.37c843b0.jpg"},
+                {"Sauce Labs Bolt T-Shirt", "$15.99", "bolt-shirt-1200x1500.c2599ac5.jpg"},
+                {"Sauce Labs Fleece Jacket", "$49.99", "sauce-pullover-1200x1500.51d7ffaf.jpg"},
+                {"Sauce Labs Onesie", "$7.99", "red-onesie-1200x1500.2ec615b2.jpg"},
+                {"Test.allTheThings() T-Shirt (Red)", "$15.99", "red-tatt-1200x1500.30dadef4.jpg"}
+        };
+
+        for (int i = 0; i< productListItems.size(); i++){
+            WebElement product = productListItems.get(i);
+            // Get actual product details
+            String actualName = product.findElement(By.className("inventory_item_name")).getText();
+            String actualPrice = product.findElement(By.className("inventory_item_price")).getText();
+//
+            String expectedName = expectedProducts[i][0];
+            String expectedPrice = expectedProducts[i][1];
+//
+            Assert.assertEquals(actualName,expectedName,"Product name mismatch for " + expectedName);
+            Assert.assertEquals(actualPrice,expectedPrice,"Product price mismatch for " + expectedName);
+        }
+
+
+    }
+
+    @Test
+    public void testVerifyProductPage() {
+        driver.findElement(By.xpath("//div[normalize-space()='Sauce Labs Backpack']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//div[normalize-space()='Sauce Labs Backpack']")).getText(),
+                "Sauce Labs Backpack", "Product method is not matching");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='inventory_details_price']")).getText(),"$29.99");
+    }
+
+    @Test
+    public void testClickAddToCartButtonForEachProduct() {
+         List<WebElement> cartButtons = driver.findElements(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
+         for (WebElement addToCartButton: cartButtons){
+             addToCartButton.click();
+         }
+         Assert.assertEquals(driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText(),"6", "Cart Item count is not matching");
+    }
+}
